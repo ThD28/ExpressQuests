932086d9222a2a067de72b16aa91a3089a0134da
Theo DuboisE <th.dubois201@gmail.comm>
2022-11-07 11:52:18 +0100

    put users

--- app.js
+++ app.js
@@ -20,10 +20,12 @@ const usersHandlers = require("./usersHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
 app.post("/api/users", usersHandlers.postUsers);
+app.put("/api/users/:id", usersHandlers.updateUsers);
 
 app.listen(port, (err) => {
   if (err) {
